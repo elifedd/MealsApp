c824faa393bd7b7f65ac92d6c1d7feffb280fbfb
Elif Duygu Demircioğlu <95955738+elifedd@users.noreply.github.com>
2024-06-19 13:10:24 +0300

    add favorite feature with redux

--- screens/FavoritesScreen.js
+++ screens/FavoritesScreen.js
@@ -1,14 +1,16 @@
-import React, { useContext } from 'react';
+import React from 'react';
 import MealsList from '../components/MealsList/MealsList';
-import { FavoritesContext } from '../store/context/favorites-context';
+import { useSelector } from 'react-redux';
+// import { FavoritesContext } from '../store/context/favorites-context';
 import { MEALS } from '../data/dummy-data';
 import { StyleSheet, View, Text } from 'react-native';
 
 export default function FavoritesScreen() {
-  const favoriteMealsCtx = useContext(FavoritesContext);
+  // const favoriteMealsCtx = useContext(FavoritesContext);
+  const favoriteMealIds = useSelector((state) => state.favoriteMeals.ids);
 
   const favoriteMeals = MEALS.filter((meal) =>
-    favoriteMealsCtx.ids.includes(meal.id)
+    favoriteMealIds.includes(meal.id)
   );
 
   if (favoriteMeals.length === 0) {
